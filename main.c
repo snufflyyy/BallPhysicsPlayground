5fe0e6053bd7076f3e2bc3ffdfc0e2bb73039a18
braden <snufflyy@proton.me>
2024-04-01 01:40:51 -0500

    verlet physics

--- main.c
+++ main.c
@@ -1,134 +1,134 @@
 #include <raylib.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <math.h>
 
-#define MAXBALLS 1000
+#define MAXBALLAMOUNT 2000
 
 typedef struct {
-    float ballRadius;
-    Vector2 ballPosition;
-    Vector2 ballVelocity;
-    Color ballColor;
+    Vector2 currentPosition;
+    Vector2 lastPosition;
+    Vector2 acceleration;
+    float radius;
+    Color color;
 } ball;
 
-const int windowWidth = 800;
-const int windowHeight = 600;
+const int windowWidth = 1280;
+const int windowHeight = 720;
 
-// world variables
-const float gravity = 0.5f;
-const float airResistance = 1;
-const float friction = 2.5f;
+const int subSteps = 2;
 
-// 1000 is the max amount of balls possible
-ball balls[MAXBALLS];
-int numberOfBalls;
+const Vector2 gravity = {0, 2500};
 
-void spawnBall() {
-    if (IsKeyPressed(KEY_SPACE) && numberOfBalls != MAXBALLS) {
-        srand(time(NULL));
-        balls[numberOfBalls].ballRadius = rand() % 25 + 1;
-        balls[numberOfBalls].ballPosition.x = (float) windowWidth / 2;
-        balls[numberOfBalls].ballPosition.y = balls[numberOfBalls].ballRadius;
-        balls[numberOfBalls].ballVelocity.x = 0;
-        balls[numberOfBalls].ballColor = BLUE;
-        numberOfBalls++;
+// 1000 is the max amount of balls allowed
+ball balls[MAXBALLAMOUNT];
+int ballAmount = 0;
+
+void createBall() {
+    if (ballAmount != MAXBALLAMOUNT) {
+        balls[ballAmount].radius = 25;
+        balls[ballAmount].currentPosition.x = windowWidth / 2;
+        balls[ballAmount].currentPosition.y = balls[ballAmount].radius * 3;
+        balls[ballAmount].lastPosition.x = balls[ballAmount].currentPosition.x;
+        balls[ballAmount].lastPosition.y = balls[ballAmount].currentPosition.y; 
+        balls[ballAmount].color = BLUE;
+        ballAmount++;
     }
 }
 
-void updateBalls() {
-    for (int i = 0; i < numberOfBalls; i++) {
-        balls[i].ballVelocity.y += gravity;
+void applyGravity() {
+    for (int i = 0; i < ballAmount; i++) {
+        balls[i].acceleration.x = gravity.x;
+        balls[i].acceleration.y = gravity.y;
+    }
+}
 
-        balls[i].ballPosition.x += balls[i].ballVelocity.x * airResistance;
-        balls[i].ballPosition.y += balls[i].ballVelocity.y * airResistance;
+void updateBalls(float deltaTime) {
+    for (int i = 0; i < ballAmount; i++) {
+        Vector2 velocity = {balls[i].currentPosition.x - balls[i].lastPosition.x, balls[i].currentPosition.y - balls[i].lastPosition.y};
 
-        // floor
-        if (balls[i].ballPosition.y > (float) windowHeight - balls[i].ballRadius) {
-            balls[i].ballPosition.y = (float) windowHeight - balls[i].ballRadius;
-            balls[i].ballVelocity.y = -balls[i].ballVelocity.y + friction;
+        balls[i].lastPosition.x = balls[i].currentPosition.x;
+        balls[i].lastPosition.y = balls[i].currentPosition.y;
+
+        balls[i].currentPosition.x = balls[i].currentPosition.x + velocity.x + balls[i].acceleration.x * deltaTime * deltaTime;
+        balls[i].currentPosition.y = balls[i].currentPosition.y + velocity.y + balls[i].acceleration.y * deltaTime * deltaTime;
+
+        balls[i].acceleration.x = 0;
+        balls[i].acceleration.y = 0;
+    }
+}
+
+void applyConstraints() {
+    for (int i = 0; i < ballAmount; i++) {
+        if (balls[i].currentPosition.y > windowHeight - balls[i].radius) {
+            balls[i].currentPosition.y = windowHeight - balls[i].radius;
+        } else if (balls[i].currentPosition.y < balls[i].radius) {
+            balls[i].currentPosition.y = balls[i].radius;
         }
 
-        if (balls[i].ballPosition.x < 0 + balls[i].ballRadius) {
-            balls[i].ballPosition.x = balls[i].ballRadius;
-            balls[i].ballVelocity.x = -balls[i].ballVelocity.x - friction;
-        }
-
-        if (balls[i].ballPosition.x > (float) windowWidth - balls[i].ballRadius) {
-            balls[i].ballPosition.x = (float) windowWidth - balls[i].ballRadius;
-            balls[i].ballVelocity.x = -balls[i].ballVelocity.x + friction;
+        if (balls[i].currentPosition.x < balls[i].radius) {
+            balls[i].currentPosition.x = balls[i].radius;
+        } else if (balls[i].currentPosition.x > windowWidth - balls[i].radius) {
+            balls[i].currentPosition.x = windowWidth - balls[i].radius;
         }
     }
+}
 
-    if (numberOfBalls > 1) {
-        for (int i = 0; i < numberOfBalls - 1; i++) {
-            for (int b = 1; b < numberOfBalls; b++) {
-                float dX = 0;
-                float dY = 0;
+void getCollisions() {
+    for (int i = 0; i < ballAmount; i++) {
+        for (int b = i + 1; b < ballAmount; b++) {
+            Vector2 collisionAxis = {balls[i].currentPosition.x - balls[b].currentPosition.x, balls[i].currentPosition.y - balls[b].currentPosition.y};
+            float distance = sqrtf(collisionAxis.x * collisionAxis.x + collisionAxis.y * collisionAxis.y);
+            float minimumDistance = balls[i].radius + balls[b].radius;
 
-                bool isBGreaterX = false;
-                bool isBGreaterY = false;
-
-                if (balls[b].ballPosition.x > balls[i].ballPosition.x) {
-                    dX = balls[b].ballPosition.x - balls[i].ballPosition.x;
-                    isBGreaterX = true;
-                } else {
-                    dX = balls[i].ballPosition.x - balls[b].ballPosition.x;
-                    isBGreaterX = false;
-                }
-
-                if (balls[b].ballPosition.y > balls[i].ballPosition.y) {
-                    dY = balls[b].ballPosition.y - balls[i].ballPosition.y;
-                    isBGreaterY = true;
-                } else {
-                    dY = balls[i].ballPosition.y - balls[b].ballPosition.y;
-                    isBGreaterY = false;
-                }
-
-                if (sqrtf(dX * dX + dY * dY) < balls[b].ballRadius + balls[i].ballRadius) {
-                    if (isBGreaterX) {
-                        balls[b].ballPosition.x += (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dX * dX)) / 2;
-                        balls[b].ballVelocity.x += 1;
-                        balls[i].ballPosition.x -= (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dX * dX)) / 2;
-                        balls[i].ballVelocity.x -= 1;
-                    } else {
-                        balls[i].ballPosition.x += (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dX * dX)) / 2;
-                        balls[b].ballVelocity.x -= 1;
-                        balls[b].ballPosition.x -= (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dX * dX)) / 2;
-                        balls[i].ballVelocity.x += 1;
-                    }
-
-                    if (isBGreaterY) {
-                        balls[b].ballPosition.y += (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dY * dY)) / 2;
-                        balls[i].ballPosition.y -= (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dY * dY)) / 2;
-                    } else {
-                        balls[i].ballPosition.y += (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dY * dY)) / 2;
-                        balls[b].ballPosition.y -= (balls[b].ballRadius + balls[i].ballRadius - sqrtf(dY * dY)) / 2;
-                    }
-                }
+            if (distance < minimumDistance) {
+                Vector2 n = {collisionAxis.x / distance, collisionAxis.y / distance};
+                float delta = minimumDistance - distance;
+                balls[i].currentPosition.x += 0.5f * delta * n.x;
+                balls[i].currentPosition.y += 0.5f * delta * n.y;
+                balls[b].currentPosition.x -= 0.5f * delta * n.x;
+                balls[b].currentPosition.y -= 0.5f * delta * n.y;
             }
-        }
+
+        }    
     }
 }
 
 int main() {
-    InitWindow(windowWidth, windowHeight, "Bouncing Ball");
-
-    SetTargetFPS(60);
+    SetTargetFPS(240);
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(windowWidth, windowHeight, "Balls");
 
     while (!WindowShouldClose()) {
-        spawnBall();
-        updateBalls();
+        const float subStepsDeltaTime = GetFrameTime() / (float) subSteps;
+
+        if (IsKeyDown(KEY_SPACE)) { createBall(); }
+
+        for (int i = subSteps; i >= 0; i--) {
+            applyGravity();
+            updateBalls(subStepsDeltaTime);
+            applyConstraints();
+            getCollisions();
+
+            for (int i = 0; i < ballAmount; i++) {
+                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && CheckCollisionPointCircle(GetMousePosition(), balls[i].currentPosition, balls[i].radius)) {
+                balls[i].currentPosition.x = GetMouseX();
+                balls[i].currentPosition.y = GetMouseY();
+                }
+            }
+        }
 
         BeginDrawing();
             ClearBackground(BLACK);
-
-            DrawFPS(25, 25);
-
-            for (int i = 0; i < numberOfBalls; i++) {
-                DrawCircle(balls[i].ballPosition.x, balls[i].ballPosition.y, balls[i].ballRadius, balls[i].ballColor);
+            
+            for (int i = 0; i < ballAmount; i++) {
+                DrawCircle(balls[i].currentPosition.x, balls[i].currentPosition.y, balls[i].radius, balls[i].color);
             }
+
+            DrawFPS(0, 0);
+            DrawText(TextFormat("%d", ballAmount), 0, 10, 25, WHITE);
         EndDrawing();
+
     }
+    
+    return 0;
 }
